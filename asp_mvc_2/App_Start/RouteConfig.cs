c23eb02862b75553d01d2667b6952c4f7df89f4e
NA <NA@DESKTOP-U1FJTID>
2017-01-21 06:42:45 +0700

    -ASP MVC 2 1st Commit

--- asp_mvc_2/App_Start/RouteConfig.cs
+++ asp_mvc_2/App_Start/RouteConfig.cs
@@ -12,7 +12,6 @@ namespace asp_mvc_2
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
